05c2374e689c58ede5350dc4ac7fd7b137f07b46
v.golle <v.golle@isispace.nl>
2020-03-31 11:48:35 +0200

    WDT works perfectly

--- pca10028/s130/config/sdk_config.h
+++ pca10028/s130/config/sdk_config.h
@@ -2829,7 +2829,7 @@
 // <e> WDT_ENABLED - nrf_drv_wdt - WDT peripheral driver
 //==========================================================
 #ifndef WDT_ENABLED
-#define WDT_ENABLED 0
+#define WDT_ENABLED 1
 #endif
 #if  WDT_ENABLED
 // <o> WDT_CONFIG_BEHAVIOUR  - WDT behavior in CPU SLEEP or HALT mode
@@ -2840,7 +2840,7 @@
 // <0=> Pause in SLEEP and HALT 
 
 #ifndef WDT_CONFIG_BEHAVIOUR
-#define WDT_CONFIG_BEHAVIOUR 1
+#define WDT_CONFIG_BEHAVIOUR 0
 #endif
 
 // <o> WDT_CONFIG_RELOAD_VALUE - Reload value  <15-4294967295> 
